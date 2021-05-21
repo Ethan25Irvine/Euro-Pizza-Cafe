563b9705e5b971c9526cb9ba026c2eaafd299b37
Ethan Irvine <edog.irvine@gmail.com>
2021-05-20 19:46:30 -0700

    Added no gutters to imagesto get edge to edge design

--- euro-pizza/src/pages/home/home.js
+++ euro-pizza/src/pages/home/home.js
@@ -14,24 +14,31 @@ import Employee from './img/employee.jpg';
 function Home() {
 
   return (
-    <div id="header" className="container-fluid">
-      <div className="row tan text-center ">
-        <div className="py-3 text-dark ">
-          <h4>With Gratitude We Welcome You to</h4>
-          <h4></h4>
-          <h1 className="text-danger display-1"><strong>EURO PIZZA CAFÈ</strong> </h1>
+    <div id="" className="">
+      <div className="row tan text-center g-0">
+        <div className="col">
+          <div className="py-3 text-dark ">
+            <h4>With Gratitude We Welcome You to</h4>
+            <h4></h4>
+            <h1 className="text-danger display-1"><strong>EURO PIZZA CAFÈ</strong> </h1>
 
-          <h4 className="text-dark">Your Gathering Place for {<br />}
-            Family, Friends and
+            <h4 className="text-dark">Your Gathering Place for {<br />}
+              Family, Friends and
             New Acquaintances</h4>
-        </div>
-        <div className="row tan text-center">
-          <p className="text-dark h5"><strong>OUR DISHES ARE DELICIOUS!</strong></p>
-          <p className="text-dark col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12  col-12">We use Only the Finest Ingredients Our Cocktails are Uniquely Crafted, Wines Hand Selected, Beers Bubbly and Cold. {<br />}Relax and Take Your Time. Inside or on the newly renovated Fountain side patio Where your furry friends are also welcome.</p>
-          {/* <p className="text-dark col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12  col-12">Relax and Take Your Time. Inside or on the newly renovated Fountain side patio Where your furry friends are also welcome.</p> */}
-          <p className="text-dark col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12  col-12 h4 ">Proudly serving Fountain Hills for more than {<br />}<strong>Twenty Years!</strong></p>
+
+            <p className="text-dark h5"><strong>OUR DISHES ARE DELICIOUS!</strong></p>
+          <p className="text-dark ">We use Only the Finest Ingredients Our Cocktails are Uniquely Crafted, Wines Hand Selected, Beers Bubbly and Cold. {<br />}Relax and Take Your Time. Inside or on the newly renovated Fountain side patio Where your furry friends are also welcome.</p>
+          <p className="text-dark h4 ">Proudly serving Fountain Hills for more than {<br />}<strong>Twenty Years!</strong></p>
+          </div>
         </div>
       </div>
+      {/* <div className="row tan text-center no-gutters">
+        <div className="col">
+          <p className="text-dark h5"><strong>OUR DISHES ARE DELICIOUS!</strong></p>
+          <p className="text-dark col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12  col-12">We use Only the Finest Ingredients Our Cocktails are Uniquely Crafted, Wines Hand Selected, Beers Bubbly and Cold. {<br />}Relax and Take Your Time. Inside or on the newly renovated Fountain side patio Where your furry friends are also welcome.</p>
+          <p className="text-dark col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12  col-12 h4 ">Proudly serving Fountain Hills for more than {<br />}<strong>Twenty Years!</strong></p>
+        </div>
+      </div> */}
       {/* <div className="row black">
         <div className="carousel slide" data-bs-ride="carousel">
           <div className="carousel-inner">
@@ -62,32 +69,40 @@ function Home() {
         </div>
       </div> */}
 
-      <div className="row black">
-        <img className="mx-auto d-block" src={Collage}></img>
-      </div>
-      <div className="row black">
-        <img className="img-fluid mx-auto d-block" src={Employee}></img>
-      </div>
-
-
-      <div className="container py-5 text-center">
-        <div className="row">
-        <p className="h3 text-center border-bottom text-light border-light">MEET MERITA</p>
+      <div className="row black g-0">
+        <div className="col">
+          <img className="img-fluid mx-auto d-block" src={Collage}></img>
+          <img className="img-fluid mx-auto d-block" src={Employee}></img>
         </div>
-          <div className="row bg-light">
-          <div className="col-lg-2 col-md-2">
-            <img className="img-fluid pt-5 mx-auto d-block" src={Merita}></img>
+      </div>
+      {/* <div className="row black no-gutters">
+        <div className="col">
+          <img className="img-fluid mx-auto d-block" src={Employee}></img>
+        </div>
+      </div> */}
+
+
+      <div className="container py-3 text-center">
+        <div className="row">
+          <div className="col">
+            <p className="h3 text-center border-bottom text-light border-light">MEET MERITA</p>
           </div>
-          <div className=" col-lg-8 col-md-8 py-5 text-dark bd-black">
-            <p className="">Merita is a modern woman who embraces the time tested European ways of life. Except for her escape from the communist regime. A civil engineer by trade, she found herself designing and building a restaurant for a client in Fountain Hills.</p>
-            <p className="">With a new family on the way, and no one to take over; Merita stepped in and created Euro Pizza Cafe, her labor of love. {<br />}Merita takes great pride in her community and supports charitable giving.</p>
+        </div>
+        <div className="row bg-light">
+          <div className="col">
+            <div className="">
+              <img className="img-fluid pt-5 mx-auto d-block" src={Merita}></img>
+            </div>
+            <div className=" py-3 text-dark bd-black">
+              <p className="">Merita is a modern woman who embraces the time tested European ways of life. Except for her escape from the communist regime. A civil engineer by trade, she found herself designing and building a restaurant for a client in Fountain Hills.</p>
+              <p className="">With a new family on the way, and no one to take over; Merita stepped in and created Euro Pizza Cafe, her labor of love. {<br />}Merita takes great pride in her community and supports charitable giving.</p>
 
-            <p className="">She also invests in our young community, inbuing them with independence, sense of service, strong work, and business ethics. {<br />}“I am so grateful and proud of all my employees. They all contributed in making Euro a great success story".</p>
+              <p className="">She also invests in our young community, inbuing them with independence, sense of service, strong work, and business ethics. {<br />}“I am so grateful and proud of all my employees. They all contributed in making Euro a great success story".</p>
 
-            <p className="">She raised four great children:{<br />}
-              “I couldn’t have done it without the community. As they say, it takes a village.”</p>
+              <p className="">She raised four great children:{<br />}
+                “I couldn’t have done it without the community. As they say, it takes a village.”</p>
+            </div>
           </div>
-
         </div>
       </div>
     </div>
